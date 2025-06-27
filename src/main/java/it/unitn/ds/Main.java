15a59cc7f86f39d60ac8ee89c3d271ae57a0d16d
AsaiAshita <114286827+AsaiAshita@users.noreply.github.com>
2025-06-27 16:48:33 +0200

    quick updates to node creation in main.java

--- src/main/java/it/unitn/ds/Main.java
+++ src/main/java/it/unitn/ds/Main.java
@@ -1,7 +1,10 @@
 package it.unitn.ds;
+
 import java.io.IOException;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
   //final static int N_SENDERS = 5;
@@ -9,16 +12,19 @@ public class Main {
   public static void main(String[] args) {
     // Create an actor system
     final ActorSystem system = ActorSystem.create("DS_Project");
+    
+    List<ActorRef> nodes = new ArrayList<>();
 
     //initial values we will give to the starting set of nodes
     int [] initial_values = {10,20,30,40,50};
 
     //create all initial actors
     for(int j = 0; j<5; j++){
-      final ActorRef receiver = system.actorOf(
+      ActorRef node = system.actorOf(
               Actor.props(initial_values[j]),    // actor class
               "node_" + j     // the new actor name (unique within the system)
       );
+      nodes.add(node);
     }
 	
     //the following is a remnant of the lab files I took inspiration from for the basis of the project
