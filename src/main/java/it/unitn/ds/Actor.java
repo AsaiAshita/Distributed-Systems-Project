7a6646df1474886dd919b42020452cd72b0d658f
AsaiAshita <114286827+AsaiAshita@users.noreply.github.com>
2025-07-11 18:53:28 +0200

    implemented update + modifications to get

--- src/main/java/it/unitn/ds/Actor.java
+++ src/main/java/it/unitn/ds/Actor.java
@@ -2,40 +2,392 @@ package it.unitn.ds;
 
 import akka.actor.*;
 
+import java.io.Serializable;
 import java.util.*;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 import it.unitn.ds.Client.GetMsg;
 import it.unitn.ds.Client.UpdateMsg;
 
-public class Actor extends AbstractActor{
+public class Actor extends AbstractActor {
 
+    // Node identifier
     private int id;
-    private final Set<ActorRef> currentView;
+
+    // Read quorum
+    private static final int R = 2;
+
+    // Write quorum
+    private static final int W = 2;
+
+    // Number of nodes storing the replica
+    private static final int N = 3; 
+
+    // List of other nodes in the system (excluding self)
+    private final ArrayList<ActorRef> currentView;
+
+    // Local key-value store: key -> (version, value)
+    private final Map<Integer, Pair<Integer,String>> values;
+
+    // Keeps track of the responses received for each read request
+    private Map<Integer, ArrayList<Pair<Integer,String>>> pendingReads = new HashMap<>();
+
+    // Maps each read key to the client actor that requested it
+    private Map<Integer, ActorRef> pendingClients = new HashMap<>();
+
+    private Map<ActorRef, Integer> id_ref_association = new HashMap<>();
+
+    // Used for introducing randomized delays in responses
+    private final Random random = new Random();
+
+    // Timeout duration for quorum wait
+    private static final int TIMEOUT_MS = 2500;
+
+    // Keeps track of which keys currently have a pending read operation
+    private Set<Integer> pendingReadOperations;
 
     public Actor(int id) {
         this.id = id;
-        this.currentView = new HashSet<>();
+        this.currentView = new ArrayList<>();
+        this.values = new HashMap<>();
+        this.pendingReadOperations = new HashSet<>();
     }
 
-    static public Props props(int id) {
-        return Props.create(
-                Actor.class,
-                () -> new Actor(id));
+    public static Props props(int id) {
+        return Props.create(Actor.class, () -> new Actor(id));
     }
 
+     // Handles a Get request from a client. If no read is pending for the key, initiate a quorum read.
     private void getValue(GetMsg getMsg){
+        //a process can issue multiple reads and writes even on the same key, thus this part
+        //is not necessary
+        //if (pendingReadOperations.contains(getMsg.key)) {
+        //    return;
+        //}
 
+        pendingReadOperations.add(getMsg.key);
+        pendingReads.put(getMsg.key, new ArrayList<>());
+        pendingClients.put(getMsg.key, getSender());
+
+        // Send InternalGetMsg to all nodes in the view
+        // Incorrect! Only need to send to the N nodes that have the value!
+        //for (int i = 0; i < currentView.size(); i++) {
+        //    currentView.get(i).tell(new Actor.InternalGetMsg(getMsg.key), getSelf());
+        //}
+
+        final ArrayList<ActorRef> nodesForGet = new ArrayList<>();
+
+        //find the nodes to which we need to send the request to
+        //here for the moment we assume that the nodes are stored in the hashmap in id order.
+        //This may be false, however, hence we need to preserve this when we
+        //update the view
+        for(int i =0; i<currentView.size(); i++){
+            if (id_ref_association.get(currentView.get(i)) >= getMsg.key){
+                nodesForGet.add(currentView.get(i));
+            }
+        }
+        if (nodesForGet.size() != N){
+            int temp = nodesForGet.size();
+            for(int i=0; i<N-temp; i++){
+                nodesForGet.add(currentView.get(i));
+            }
+        }
+
+        for (int i = 0; i < nodesForGet.size(); i++) {
+            nodesForGet.get(i).tell(new Actor.InternalGetMsg(getMsg.key), getSelf());
+        }
+
+        // Send to the coordinator as well
+        // Note: it's useless, because the coordinator itself should be in the view
+        // hence if it has the value it will automatically deal with it due to the
+        // mechanism implemented above
+        // getSelf().tell(new Actor.InternalGetMsg(getMsg.key), getSelf());
+
+        // Schedule a timeout in case not enough responses arrive in time
+        getContext().getSystem().scheduler().scheduleOnce(
+            scala.concurrent.duration.Duration.create(TIMEOUT_MS, "milliseconds"),
+            getSelf(),
+            new Timeout(getMsg.key),
+            getContext().getDispatcher(),
+            ActorRef.noSender()
+        );
     }
+
+    // Handles an InternalGetMsg by replying with the local value (if exists) after a random delay.
+    private void handleInternalGet(InternalGetMsg msg) {
+        if (values.containsKey(msg.key)) {
+            Pair<Integer, String> pair = values.get(msg.key);
+            int delayMs = 100 + random.nextInt(2901); // Delay between 100ms and 3000ms
+
+            ActorRef originalSender = getSender();
+
+            getContext().getSystem().scheduler().scheduleOnce(
+                scala.concurrent.duration.Duration.create(delayMs, "milliseconds"),
+                () -> originalSender.tell(new Actor.ReceiveMsg(msg.key, pair.getLeft(), pair.getRight()), getSelf()),
+                getContext().getDispatcher()
+            );
+        }
+    }
+
+
+    // Handles ReceiveMsg replies from nodes. If enough responses are received, selects the one with the highest version and returns it to the client.
+    private void receiveResponses(ReceiveMsg msg) {
+        pendingReads.get(msg.key).add(Pair.of(msg.version, msg.value));
+
+        if (pendingReads.get(msg.key).size() >= R && pendingReadOperations.contains(msg.key)) {
+            Pair<Integer, String> best = pendingReads.get(msg.key).stream()
+                .max(Comparator.comparingInt(Pair::getLeft)) // choose highest version
+                .orElse(null);
+
+            if (best != null) {
+                pendingClients.get(msg.key).tell(new SendMsg(best.getRight()), getSelf());
+                pendingReadOperations.remove(msg.key);
+            }
+        }
+    }
+
 
     private void updateValue(UpdateMsg updateMsg){
+        //a process can issue multiple reads and writes even on the same key, thus this part
+        //is not necessary
+        //if (pendingReadOperations.contains(updateMsg.key)) {
+        //    return;
+        //}
 
+        pendingReadOperations.add(updateMsg.key);
+        pendingReads.put(updateMsg.key, new ArrayList<>());
+        pendingClients.put(updateMsg.key, getSender());
+
+        //we create a list of nodes that we need to send the message to
+        final ArrayList<ActorRef> nodesForGet = new ArrayList<>();
+
+        //find the nodes to which we need to send the request to
+        //here for the moment we assume that the nodes are stored in the hashmap in id order.
+        //This may be false, however, hence we need to preserve this when we
+        //update the view
+        for(int i =0; i<currentView.size(); i++){
+            if (id_ref_association.get(currentView.get(i)) >= updateMsg.key){
+                nodesForGet.add(currentView.get(i));
+            }
+        }
+        if (nodesForGet.size() != N){
+            int temp = nodesForGet.size();
+            for(int i=0; i<N-temp; i++){
+                nodesForGet.add(currentView.get(i));
+            }
+        }
+
+        for (int i = 0; i < nodesForGet.size(); i++) {
+            nodesForGet.get(i).tell(new Actor.InternalUpdateMsg(updateMsg.key, updateMsg.value, nodesForGet), getSelf());
+        }
+
+        // Schedule a timeout in case not enough responses arrive in time
+        getContext().getSystem().scheduler().scheduleOnce(
+                scala.concurrent.duration.Duration.create(TIMEOUT_MS, "milliseconds"),
+                getSelf(),
+                new TimeoutW(updateMsg.key, updateMsg.value),
+                getContext().getDispatcher(),
+                ActorRef.noSender()
+        );
     }
+
+    // Handles an InternalUpdateMsg by replying with the local value (if exists) after a random delay.
+    private void handleInternalUpdateGet(InternalUpdateMsg msg) {
+        if (values.containsKey(msg.key)) {
+            Pair<Integer, String> pair = values.get(msg.key);
+            int delayMs = 100 + random.nextInt(2901); // Delay between 100ms and 3000ms
+
+            ActorRef originalSender = getSender();
+
+            getContext().getSystem().scheduler().scheduleOnce(
+                    scala.concurrent.duration.Duration.create(delayMs, "milliseconds"),
+                    () -> originalSender.tell(new Actor.ReceiveUpdMsg(msg.key, pair.getLeft(), pair.getRight(), msg.value_to_update, msg.nodes), getSelf()),
+                    getContext().getDispatcher()
+            );
+        }
+    }
+
+    // Handles the update process. When enough (>=W) messages have been received, it sneds a succes
+    // message to the client and then it starts the updating process
+    private void handleUpdate(ReceiveUpdMsg msg) {
+        pendingReads.get(msg.key).add(Pair.of(msg.version, msg.value));
+
+        if (pendingReads.get(msg.key).size() >= W && pendingReadOperations.contains(msg.key)) {
+            Pair<Integer, String> best = pendingReads.get(msg.key).stream()
+                    .max(Comparator.comparingInt(Pair::getLeft)) // choose highest version
+                    .orElse(null);
+
+            if (best != null) {
+                pendingClients.get(msg.key).tell(new SendMsg("Successful insertion of value " + msg.value_to_update + " into node of key " + String.valueOf(msg.key) + "\n"), getSelf());
+                int versionUpdate = best.getLeft();
+                versionUpdate = versionUpdate + 1;
+                for (int i=0; i<msg.nodes.size(); i++){
+                    msg.nodes.get(i).tell(new Actor.NewUpdate(msg.key, versionUpdate, msg.value_to_update), getSelf());
+                }
+                pendingReadOperations.remove(msg.key);
+            }
+        }
+    }
+
+    private void writeUpdate(NewUpdate msg){
+        if(values.containsKey(msg.key)){
+            values.put(msg.key, Pair.of(msg.version, msg.value));
+        }
+    }
+
+    // Called when timeout occurs for a pending read. If quorum was not reached, responds to the client with null.
+    private void onTimeoutRead(Timeout timeout) {
+        if (pendingReads.get(timeout.key).size() < R && pendingReadOperations.contains(timeout.key)) {
+            pendingClients.get(timeout.key).tell(new SendMsg("Read of value failed"), getSelf());
+            pendingReads.get(timeout.key).clear();
+        }
+    }
+
+    // Called when timeout occurs for a pending write. If quorum was not reached, responds to the client with null.
+    private void onTimeoutWrite(TimeoutW timeout) {
+        if (pendingReads.get(timeout.key).size() < R && pendingReadOperations.contains(timeout.key)) {
+            pendingClients.get(timeout.key).tell(new SendMsg("Write of value " + timeout.value + " failed"), getSelf());
+            pendingReads.get(timeout.key).clear();
+        }
+    }
+
+    
+    // Updates the view of known nodes (excluding itself)
+    private void updateView(UpdateView msg) {
+        for (ActorRef node : msg.nodes) {
+            if (!node.equals(getSelf())) { 
+                currentView.add(node);
+            }
+        }
+    }
+
+    // Sets the local key-value store with initial values.
+    private void setValues(SetValues msg) {
+        this.values.putAll(msg.values);
+    }
+
+    private void setIdAssociation(SetIdAssociation msg){
+        this.id_ref_association = msg.map;
+    }
+
+    // ---- Message classes below ----
+
+    public static class Timeout implements Serializable {
+        public final int key;
+        public Timeout(int key) {
+            this.key = key;
+        }
+    }
+
+    public static class TimeoutW implements Serializable {
+        public final int key;
+        public final String value;
+        public TimeoutW(int key, String value) {
+            this.key = key; this.value = value;
+        }
+    }
+
+    public static class UpdateView implements Serializable {
+        public final ArrayList<ActorRef> nodes;
+        public UpdateView(ArrayList<ActorRef> nodes) {
+            this.nodes = nodes;
+        }
+    }
+
+    public static class SetValues implements Serializable {
+        public final Map<Integer,Pair<Integer,String>> values;
+        public SetValues(Map<Integer,Pair<Integer,String>> values) {
+            this.values = values;
+        }
+    }
+
+    public static class SetIdAssociation implements Serializable {
+        public final Map<ActorRef, Integer> map;
+        public SetIdAssociation(Map<ActorRef, Integer> map) {
+            this.map = map;
+        }
+    }
+
+    public static class SendMsg implements Serializable {
+        public String value = new String();
+        public SendMsg(String value) {
+            this.value = value;
+        }
+    }
+
+    public static class InternalGetMsg implements Serializable {
+        public final int key;
+        public InternalGetMsg(int key) {
+            this.key = key;
+        }
+    }
+
+    public static class InternalUpdateMsg implements Serializable {
+        public final int key;
+        public final String value_to_update;
+        public final ArrayList<ActorRef> nodes;
+        public InternalUpdateMsg(int key, String value_to_update, ArrayList<ActorRef> nodes) {
+            this.key = key;
+            this.value_to_update = value_to_update;
+            this.nodes = nodes;
+        }
+    }
+
+    public static class ReceiveMsg implements Serializable {
+        public int key;
+        public int version;
+        public String value;
+        public ReceiveMsg(int key, int version, String value) {
+            this.key = key;
+            this.version = version;
+            this.value = value;
+        }
+    }
+
+    public static class ReceiveUpdMsg implements Serializable {
+        public int key;
+        public int version;
+        public String value;
+        public String value_to_update;
+        public ArrayList<ActorRef> nodes;
+        public ReceiveUpdMsg(int key, int version, String value, String value_to_update, ArrayList<ActorRef> nodes) {
+            this.key = key;
+            this.version = version;
+            this.value = value;
+            this.value_to_update = value_to_update;
+            this.nodes = nodes;
+        }
+    }
+
+    public static class NewUpdate implements Serializable{
+        public int key;
+        public int version;
+        public String value;
+
+        public NewUpdate(int key, int version, String value) {
+            this.key = key;
+            this.version = version;
+            this.value = value;
+        }
+    }
+
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .match(GetMsg.class, this::getValue)
                 .match(UpdateMsg.class, this::updateValue)
+                .match(UpdateView.class, this::updateView)
+                .match(SetValues.class, this::setValues)
+                .match(InternalGetMsg.class, this::handleInternalGet)
+                .match(ReceiveMsg.class, this::receiveResponses)
+                .match(Timeout.class, this::onTimeoutRead)
+                .match(TimeoutW.class, this::onTimeoutWrite)
+                .match(InternalUpdateMsg.class, this::handleInternalUpdateGet)
+                .match(ReceiveUpdMsg.class, this::handleUpdate)
+                .match(SetIdAssociation.class, this::setIdAssociation)
+                .match(NewUpdate.class, this::writeUpdate)
                 .build();
     }
 }
--- src/main/java/it/unitn/ds/Main.java
+++ src/main/java/it/unitn/ds/Main.java
@@ -3,17 +3,39 @@ package it.unitn.ds;
 import java.io.IOException;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-import java.util.ArrayList;
-import java.util.List;
 
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+ 
 public class Main {
   //final static int N_SENDERS = 5;
 
-  public static void main(String[] args) {
+  public static Map<ActorRef, Integer> sortByValue(Map<ActorRef, Integer> map) {
+    // Convert map entries to a list
+    List<Map.Entry<ActorRef, Integer>> entryList = new ArrayList<>(map.entrySet());
+
+    // Sort the list by value
+    Collections.sort(entryList, (entry1, entry2) -> entry1.getValue().compareTo(entry2.getValue()));
+
+    // Create a new LinkedHashMap to keep the sorted order
+    Map<ActorRef, Integer> sortedMap = new LinkedHashMap<>();
+    for (Map.Entry<ActorRef, Integer> entry : entryList) {
+      sortedMap.put((entry.getKey()), entry.getValue());
+    }
+
+    return sortedMap;
+  }
+
+  public static void main(String[] args) throws InterruptedException {
     // Create an actor system
     final ActorSystem system = ActorSystem.create("DS_Project");
     
-    List<ActorRef> nodes = new ArrayList<>();
+    ArrayList<ActorRef> nodes = new ArrayList<>();
+
+    Map<ActorRef, Integer> id_ref_association = new HashMap<>();
 
     //initial values we will give to the starting set of nodes
     int [] initial_values = {10,20,30,40,50};
@@ -25,8 +47,97 @@ public class Main {
               "node_" + j     // the new actor name (unique within the system)
       );
       nodes.add(node);
+      id_ref_association.put(node, initial_values[j]);
     }
-	
+
+    id_ref_association = sortByValue(id_ref_association);
+    System.out.print(id_ref_association);
+
+    // Create a set of values for the nodes. Here we assume N=3.
+    // However, per the project, N should be configurable at compile time, so
+    // this part needs some rework to be able to do it
+    Map<Integer, Pair<Integer, String>> valuesNode0 = new HashMap<>();
+    valuesNode0.put(4, Pair.of(1, "val4"));
+    valuesNode0.put(9, Pair.of(1, "val9"));
+    valuesNode0.put(45, Pair.of(1, "val45"));
+
+
+    Map<Integer, Pair<Integer, String>> valuesNode1 = new HashMap<>();
+    valuesNode1.put(4, Pair.of(2, "val4"));
+    valuesNode1.put(9, Pair.of(1, "val9"));
+    valuesNode1.put(11, Pair.of(1, "val11"));
+    valuesNode1.put(45, Pair.of(1, "val45"));
+
+
+
+    Map<Integer, Pair<Integer, String>> valuesNode2 = new HashMap<>();
+    valuesNode2.put(4, Pair.of(1, "val4"));
+    valuesNode2.put(9, Pair.of(1, "val9"));
+    valuesNode2.put(11, Pair.of(1, "val11"));
+    valuesNode2.put(24, Pair.of(2, "val24"));
+    valuesNode2.put(29, Pair.of(1, "val29"));
+
+
+    Map<Integer, Pair<Integer, String>> valuesNode3 = new HashMap<>();
+    valuesNode3.put(11, Pair.of(1, "val11"));
+    valuesNode3.put(24, Pair.of(1, "val24"));
+    valuesNode3.put(29, Pair.of(1, "val29"));
+
+
+    Map<Integer, Pair<Integer, String>> valuesNode4 = new HashMap<>();
+    valuesNode4.put(24, Pair.of(1, "val24"));
+    valuesNode4.put(29, Pair.of(1, "val29"));
+    valuesNode4.put(45, Pair.of(1, "val45"));
+
+
+
+    ArrayList<Map<Integer, Pair<Integer, String>>> allValues = new ArrayList<>();
+    allValues.add(valuesNode0);
+    allValues.add(valuesNode1);
+    allValues.add(valuesNode2);
+    allValues.add(valuesNode3);
+    allValues.add(valuesNode4);
+
+    // Update the actors view and values stored
+    for (int i = 0; i < nodes.size(); i++) {
+        ActorRef node = nodes.get(i);
+        node.tell(new Actor.UpdateView(nodes), ActorRef.noSender());
+        node.tell(new Actor.SetValues(allValues.get(i)), ActorRef.noSender());
+        node.tell(new Actor.SetIdAssociation(id_ref_association), ActorRef.noSender());
+    }
+
+    // Create a client
+    ActorRef client0 = system.actorOf(Client.props(0,nodes), "client_0");
+    ActorRef client1 = system.actorOf(Client.props(1,nodes), "client_1");
+    ActorRef client2 = system.actorOf(Client.props(2,nodes), "client_2");
+    ActorRef client3 = system.actorOf(Client.props(3,nodes), "client_3");
+
+    //Simulate an execution of some update and get operations.
+    //They were written randomly, so they may not be the best when testing the system.
+    //However, for the time being, it illustrates whether the system works or not.
+    client1.tell(new Client.GetMsg(4), client1);
+    client3.tell(new Client.GetMsg(9), client3);
+
+    client0.tell(new Client.UpdateMsg(11, "Antananarivo"), client0);
+    client1.tell(new Client.UpdateMsg(4, "La Paz"), client1);
+    client1.tell(new Client.UpdateMsg(9, "Victoria"), client1);
+    client2.tell(new Client.GetMsg(9), client2);
+    client3.tell(new Client.UpdateMsg(45, "Lumezzane"), client3);
+    client3.tell(new Client.UpdateMsg(24, "Oslo"), client3);
+
+    TimeUnit.SECONDS.sleep(4);
+
+    client0.tell(new Client.GetMsg(11), client0);
+    client0.tell(new Client.UpdateMsg(11, "America"), client0);
+    client1.tell(new Client.UpdateMsg(45, "Perugia"), client1);
+    client1.tell(new Client.UpdateMsg(4, "Sendai"), client1);
+    client3.tell(new Client.GetMsg(45), client3);
+    client2.tell(new Client.GetMsg(24), client2);
+
+    TimeUnit.SECONDS.sleep(2);
+
+    client0.tell(new Client.GetMsg(24), client0);
+
     //the following is a remnant of the lab files I took inspiration from for the basis of the project
     System.out.println(">>> Press ENTER to exit <<<");
     try {
